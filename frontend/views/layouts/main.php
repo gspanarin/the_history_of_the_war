5f6cfa33abb989add3eeca9cdac5234614f673ea
Панарин Геннадий Сергеевич <Panarin@avd-system.ru>
2024-08-30 17:44:23 +0100

    зміни у дизайні для фронту

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -13,52 +13,57 @@ use yii\bootstrap4\NavBar;
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="uk">
   <head>
-    <title>Haircare - Free Bootstrap 4 Template by Colorlib</title>
+    <title>Проєкт «Архівування документів про війну»</title>
+	<meta name="description" content="Архівування документів про війну Харківська державна наукова бібліотека імені В.Г Короленка">
+	<meta name="author" content="ХДНБ ім. В. Г. Короленка">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <?php $this->registerCsrfMetaTags() ?>
+	<?php $this->registerCsrfMetaTags() ?>
 	<?php $this->head() ?>
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="/assets_new/css/open-iconic-bootstrap.min.css">
-    <link rel="stylesheet" href="/assets_new/css/animate.css">
+    <link rel="stylesheet" href="assets_new/css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="assets_new/css/animate.css">
     
-    <link rel="stylesheet" href="/assets_new/css/owl.carousel.min.css">
-    <link rel="stylesheet" href="/assets_new/css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="/assets_new/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets_new/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets_new/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="assets_new/css/magnific-popup.css">
 
-    <link rel="stylesheet" href="/assets_new/css/aos.css">
+    <link rel="stylesheet" href="assets_new/css/aos.css">
 
-    <link rel="stylesheet" href="/assets_new/css/ionicons.min.css">
+    <link rel="stylesheet" href="assets_new/css/ionicons.min.css">
 
-    <link rel="stylesheet" href="/assets_new/css/bootstrap-datepicker.css">
-    <link rel="stylesheet" href="/assets_new/css/jquery.timepicker.css">
+    <link rel="stylesheet" href="assets_new/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="assets_new/css/jquery.timepicker.css">
 
     
-    <link rel="stylesheet" href="/assets_new/css/flaticon.css">
-    <link rel="stylesheet" href="/assets_new/css/icomoon.css">
-    <link rel="stylesheet" href="/assets_new/css/style.css">
+    <link rel="stylesheet" href="assets_new/css/flaticon.css">
+    <link rel="stylesheet" href="assets_new/css/icomoon.css">
+    <link rel="stylesheet" href="assets_new/css/style.css">
+      
+	<link rel="icon" href="favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
   </head>
   <body>
 <?php $this->beginBody() ?>
-	  <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+	  	  <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
 	    <div class="container">
-	      <a class="navbar-brand" href="#">Logo</a>
+	      <a class="navbar-brand" href="https://korolenko.kharkov.com/"><img src="assets_new/images/logo.svg" width="25" alt=""/>  ХДНБ ім. В.Г.Короленка</a>
 	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-	        <span class="oi oi-menu"></span> Menu
+	        <span class="oi oi-menu"></span> Меню
 	      </button>
 
 	      <div class="collapse navbar-collapse" id="ftco-nav">
 	        <ul class="navbar-nav ml-auto">
-	        	<li class="nav-item active"><a href="/" class="nav-link">Головна</a></li>
-	        	<li class="nav-item"><a href="/" class="nav-link">Про проєкт</a></li>
-	        	<li class="nav-item"><a href="/" class="nav-link">Колекції</a></li>
-	        	<li class="nav-item"><a href="/" class="nav-link">Користівачам</a></li>
-	          <li class="nav-item"><a href="/" class="nav-link">Контакти</a></li>
+	        	<li class="nav-item active"><a href="#" class="nav-link">Головна</a></li>
+	        	<li class="nav-item"><a href="#" class="nav-link">Про проєкт</a></li>
+	        	<li class="nav-item"><a href="#" class="nav-link">Колекції</a></li>
+	        	<li class="nav-item"><a href="#" class="nav-link">Користівачам</a></li>
+	          <li class="nav-item"><a href="#" class="nav-link">Контакти</a></li>
 	        </ul>
 	      </div>
 	    </div>
@@ -68,13 +73,14 @@ use yii\bootstrap4\NavBar;
 	
 	<main role="main" class="flex-shrink-0">
 		<div class="container">
-			<section class="hero-wrap hero-wrap-2" style="background-image: url('/assets_new/images/bg-1.jpg');" data-stellar-background-ratio="0.5">
+			<section class="hero-wrap hero-wrap-2" style="background-image: url('assets_new/images/bg-1.png');" data-stellar-background-ratio="0.5">
 				<div class="overlay"></div>
 				<div class="container">
 					<div class="row no-gutters slider-text js-fullheight align-items-end justify-content-center">
 						<div class="col-md-9 ftco-animate pb-5 text-center">
+							<p class="font-weight-light text-white text-uppercase">проєкт</p>
 							<h1 class="mb-0 bread">Архівування документів про війну</h1>
-							<p class="breadcrumbs"><span class="mr-2"><a href="#">Історія війни <i class="ion-ios-arrow-round-forward"></i></a></span> <span>page <i class="ion-ios-arrow-round-forward"></i></span></p>
+							<p class="subheading  text-white text-uppercase">Ініціатор – ВГО Українська бібліотечна асоціація</p>
 						</div>
 					</div>
 				</div>
