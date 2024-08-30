3aeda0ede0afb420439e3abdb91ccc71759ddbf1
Панарин Геннадий Сергеевич <Panarin@avd-system.ru>
2024-08-30 18:41:22 +0100

    змінив налаштування пошти

--- common/config/main.php
+++ common/config/main.php
@@ -18,6 +18,19 @@ return [
             'password' => env('DB_PASS'),
             'charset' => 'utf8mb4',
         ],
+		'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+			'transport' => [
+				'class' => 'Swift_SmtpTransport',
+				'host' => env('COMMON.EMAIL.HOST'),
+				'username' => env('COMMON.EMAIL.USERNAME'),
+				'password' => env('COMMON.EMAIL.PASSWORD'),
+				'port' => env('COMMON.EMAIL.PORT'),
+				//'encryption' => 'ssl',
+			],
+			'useFileTransport' => false,
+        ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
