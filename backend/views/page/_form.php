1d9880beeb1ce18402da3820f168ecfc7b7f65ee
Панарин Геннадий Сергеевич <Panarin@avd-system.ru>
2024-12-03 08:14:17 +0000

    налаштування модуля редагування сторінок

--- backend/views/page/_form.php
+++ backend/views/page/_form.php
@@ -3,7 +3,7 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use dosamigos\tinymce\TinyMce;
-
+use yii\web\JsExpression;
 /** @var yii\web\View $this */
 /** @var common\models\Page $model */
 /** @var yii\widgets\ActiveForm $form */
@@ -22,15 +22,54 @@ use dosamigos\tinymce\TinyMce;
     <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'body')->widget(TinyMce::className(), [
-        'options' => ['rows' => 6],
+        'options' => ['rows' => 12],
         'language' => 'uk',
         'clientOptions' => [
+			
+			
+			
+			'file_picker_callback' => new JsExpression("function(cb, value, meta) {
+				var input = document.createElement('input');
+				input.setAttribute('type', 'file');
+				input.setAttribute('accept', 'image/*');
+
+				// Note: In modern browsers input[type=\"file\"] is functional without 
+				// even adding it to the DOM, but that might not be the case in some older
+				// or quirky browsers like IE, so you might want to add it to the DOM
+				// just in case, and visually hide it. And do not forget do remove it
+				// once you do not need it anymore.
+
+				input.onchange = function() {
+				  var file = this.files[0];
+
+				  var reader = new FileReader();
+				  reader.onload = function () {
+					// Note: Now we need to register the blob in TinyMCEs image blob
+					// registry. In the next release this part hopefully won't be
+					// necessary, as we are looking to handle it internally.
+					var id = 'blobid' + (new Date()).getTime();
+					var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+					var base64 = reader.result.split(',')[1];
+					var blobInfo = blobCache.create(id, file, base64);
+					blobCache.add(blobInfo);
+
+					// call the callback and populate the Title field with the file name
+					cb(blobInfo.blobUri(), { title: file.name });
+				  };
+				  reader.readAsDataURL(file);
+				};
+
+				input.click();
+			   }"),
+			
+			
             'plugins' => [
                 "advlist autolink lists link charmap print preview anchor",
                 "searchreplace visualblocks code fullscreen",
                 "insertdatetime media table contextmenu paste",
                 "image",
-                "link"
+                "link",
+				"table",
             ],
             'toolbar' => "undo redo | styles | fontsize | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
             'a11y_advanced_options' => true,
