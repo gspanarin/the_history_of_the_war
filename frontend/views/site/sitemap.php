c8f824fa16c702972d017ac37e9adbc55ddc901a
Gennadij Panarin <gspanarin@gmail.com>
2025-01-27 12:26:24 +0200

    оновлення файлу виду

--- frontend/views/site/sitemap.php
+++ frontend/views/site/sitemap.php
@@ -4,10 +4,7 @@
 use yii\helpers\Url;
 ?>
 <urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
-	<?php foreach ($pages as $page): ?>
-        <url>
-            <loc><?= Url::home(true) ?><?= $page['url']?></loc>
-            <lastmod><?= $page['lastmod'] ?></lastmod>
-        </url>
-    <?php endforeach; ?>
+<?php foreach ($pages as $page): ?>
+	<url><loc><?= Url::home(true) ?><?= $page['url']?></loc><lastmod><?= $page['lastmod'] ?></lastmod></url>
+<?php endforeach; ?>
 </urlset>
