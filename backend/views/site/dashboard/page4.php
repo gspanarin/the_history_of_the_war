3c5198b7ba19a42718359139299ad5435bc5e5b1
Hennadii Panarin <gspanarin@gmail.com>
2025-05-06 09:47:27 +0300

    Виправив одруківку у адмінці

--- backend/views/site/dashboard/page4.php
+++ backend/views/site/dashboard/page4.php
@@ -2,7 +2,7 @@
 	<div class="col-md-6">
             <div class="card">
                 <div class="card-header">
-                <h3 class="card-title">Статистика за Адміністартивними одиницями</h3>
+                <h3 class="card-title">Статистика за Адміністрaтивними одиницями</h3>
                 </div>
 
                 <div class="card-body p-0">
